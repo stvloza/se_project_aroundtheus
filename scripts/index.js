6faaf95ffc972c9a89112d12ff37d0a9862d2130
Esteban Loza <stvloza@gmail.com>
2023-03-29 23:34:32 -0500

    first array

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const initialCards = [
+  {
+    title: "Yosemite Valley",
+    link: "practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+
+  {
+    title: "Lake Louise",
+    link: "practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    title: "Bald Mountains",
+    link: "practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    title: "Latemar",
+    link: "practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+
+  {
+    name: "Lago di Braies",
+    link: "practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+console.log(initialCards);
