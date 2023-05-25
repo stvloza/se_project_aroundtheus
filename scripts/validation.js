1f1e48c308825edd9055a80f23a2b2b96e7ee4a5
Esteban Loza <stvloza@gmail.com>
2023-05-25 16:33:14 -0500

    fix button state

--- scripts/validation.js
+++ scripts/validation.js
@@ -63,7 +63,7 @@ const config = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
+  inactiveButtonClass: "popup__button-disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
 };
