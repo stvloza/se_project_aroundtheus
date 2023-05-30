72815d5171f9eec1cad68641d92662298e2af073
Esteban Loza <stvloza@gmail.com>
2023-05-30 08:58:41 -0500

    fix submit button

--- scripts/validation.js
+++ scripts/validation.js
@@ -62,7 +62,7 @@ const enableValidation = (options) => {
 const config = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
+  submitButtonSelector: ".form__button-submit",
   inactiveButtonClass: "popup__button-disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
